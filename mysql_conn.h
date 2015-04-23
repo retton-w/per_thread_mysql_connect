12a225d9415e8f61a2c4121d80ba6145dacba798
Retton <hfutwangyd@gmail.com>
2015-04-23 15:42:46 +0800

    add head file

---
+++ mysql_conn.h
@@ -0,0 +1,32 @@
+/** 
+ * @file:       mysql_conn.h
+ * @brief:      posix thread specified mysql connection  
+ * @author:     retton
+ * @date:       2015-04-23
+ * @version:    V1.0.0
+ * @note:       History:
+ * @note:       <author><time><version><description>
+ * @warning:    
+ */
+#ifndef __MYSQL_CONN_H__
+#define __MYSQL_CONN_H__
+
+#include <mysql/mysql.h>
+
+#define NAME_LEN (256)
+
+/** 
+ * @name:   my_serv_info
+ * @brief:  required information to connect mysqld
+ */
+struct my_serv_info {
+    char    serv_host[NAME_LEN];
+    char    serv_user[NAME_LEN];
+    char    serv_passwd[NAME_LEN];
+    char    serv_database[NAME_LEN];
+    int     serv_port;
+};
+
+MYSQL *get_mysql(struct my_serv_info *info);
+
+#endif ///< __MYSQL_CONN_H__
